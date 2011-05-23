9e8d80258994588dd4db63cad5fb3d2f45769ae8
Pat Notz <patnotz@gmail.com>
2011-05-22 22:47:10 -0600

    rewrite to support binary ops with POD types
    Introduce a wrapper template called term<T> that has specializations
    for POD types, FastArray and (so far) the binary operations of
    +,-,*,/.
    
    Also expanded the unit tests some more. Added better and safer
    constructors and assignment operators.

--- src/FastArray.hpp
+++ src/FastArray.hpp
@@ -6,46 +6,132 @@ namespace fa {
 typedef double ScalarT;
 typedef unsigned IndexT;
 
-struct FastArray {
-public:
-  FastArray() : m_x(0) {}
-  FastArray(const IndexT size) : m_x(0), m_size(size) {
-    m_x = new ScalarT[size];
+//
+// Type promotion -- helper template for deducing
+// the appropriate return type for operations
+// involving different types
+//
+template <class T1, class T2> struct promote {};
+template <class T> struct promote<T,T> {
+  typedef T type;
+};
+#define FA_PROMOTE(TYPE1,TYPE2,RESULT_TYPE) \
+  template <> struct promote<TYPE1,TYPE2> { \
+    typedef RESULT_TYPE type; \
+  }; \
+  template <> struct promote<TYPE2,TYPE1> { \
+    typedef RESULT_TYPE type; \
+  };
+
+FA_PROMOTE(double,float,double);
+FA_PROMOTE(double,int,double);
+FA_PROMOTE(double,long,double);
+FA_PROMOTE(double,unsigned,double);
+FA_PROMOTE(float,int,float);
+FA_PROMOTE(float,long,float);
+FA_PROMOTE(float,unsigned,float);
+#undef FA_PROMOTE
+
+//
+// Term wrapper -- helper template that allows
+// POD types, FastArrays and Expression Templates
+// to be treated polymorphically
+//
+template <class T>
+struct term {
+  typedef term<T> TermT;
+  typedef typename T::ValueT ValueT;
+  // implicit constructor
+  term(const T & t) : m_t(t) {}
+  ScalarT operator[](const IndexT i) const {
+    return m_t[i];
   }
+  const T & m_t;
+};
+
+
+//
+// FastArray - array class with Expression Template
+// support and designed for SIMD vectorization
+//
+struct FastArray {
+  typedef ScalarT ValueT;
+
+  FastArray() : m_x(0), m_size(0) {}
+
+  FastArray(const IndexT size) : m_x(0), m_size(size) {
+    m_x = new ScalarT[m_size];
+    // not initialized for efficiency
+  }
+
+  FastArray(const IndexT size, const ScalarT val) : m_x(0), m_size(size) {
+    m_x = new ScalarT[m_size];
+    for(IndexT i=0; i < m_size; ++i)
+      m_x[i] = val;
+  }
+
+  FastArray(const FastArray & other) {
+    m_size = other.m_size;
+    m_x = new ScalarT[m_size];
+    for(IndexT i=0; i < m_size; ++i)
+      m_x[i] = other.m_x[i];
+  }
+
+  FastArray & operator=(const FastArray & other) {
+    if(this == &other)
+      return *this;
+
+    // need to resize : FIXME: size vs. capacity
+    if (m_size != other.m_size) {
+      if (m_x != 0) delete[] m_x;
+      m_size = other.m_size;
+      m_x = new ScalarT[m_size];
+    }
+    for(IndexT i=0; i < m_size; ++i)
+      m_x[i] = other.m_x[i];
+    return *this;
+  }
+
   ~FastArray() {
     if ( m_x != 0 ) delete[] m_x;
   }
 
-  template <class RHS>
-  FastArray & operator=(const RHS & rhs) {
+  FastArray & operator=(const ScalarT & value) {
+    for(IndexT i=0; i < m_size; ++i)
+      m_x[i] = value;
+    return *this;
+  }
+
+  template <class T>
+  FastArray & operator=(const term<T> & rhs) {
     for(IndexT i=0; i < m_size; ++i)
       m_x[i] = rhs[i];
     return *this;
   }
 
-  template <class RHS>
-  FastArray & operator+=(const RHS & rhs) {
+  template <class T>
+  FastArray & operator+=(const term<T> & rhs) {
     for(IndexT i=0; i < m_size; ++i)
       m_x[i] += rhs[i];
     return *this;
   }
 
-  template <class RHS>
-  FastArray & operator-=(const RHS & rhs) {
+  template <class T>
+  FastArray & operator-=(const term<T> & rhs) {
     for(IndexT i=0; i < m_size; ++i)
       m_x[i] -= rhs[i];
     return *this;
   }
 
-  template <class RHS>
-  FastArray & operator*=(const RHS & rhs) {
+  template <class T>
+  FastArray & operator*=(const term<T> & rhs) {
     for(IndexT i=0; i < m_size; ++i)
       m_x[i] *= rhs[i];
     return *this;
   }
 
-  template <class RHS>
-  FastArray & operator/=(const RHS & rhs) {
+  template <class T>
+  FastArray & operator/=(const term<T> & rhs) {
     for(IndexT i=0; i < m_size; ++i)
       m_x[i] /= rhs[i];
     return *this;
@@ -57,70 +143,69 @@ public:
   const ScalarT & operator[](const IndexT i) const {
     return m_x[i];
   }
-
-
+  IndexT size() const {
+    return m_size;
+  }
 private:
   ScalarT * m_x;
   IndexT m_size;
 };
 
-template <class L, class OpTag, class R>
-struct BinaryOp {
-  BinaryOp(const L & left_, const R & right_) :
-    left(left_), right(right_) {}
-
-  ScalarT operator[](IndexT i) const {
-    return OpTag::apply(left[i],right[i]);
+template <>
+struct term<FastArray> {
+  typedef term<FastArray> TermT;
+  typedef ScalarT ValueT;
+  // implicit constructor
+  term(const FastArray & fa) : m_fa(fa) {}
+  const ScalarT & operator[](const IndexT i) const {
+    return m_fa[i];
   }
-
-  const L & left;
-  const R & right;
+  const FastArray & m_fa;
 };
 
-struct plus {
-  static ScalarT apply(const ScalarT a, const ScalarT b) {
-    return a + b;
+template <>
+struct term<ScalarT> {
+  typedef term<ScalarT> TermT;
+  typedef ScalarT ValueT;
+  // implicit constructor
+  term(const ScalarT c) : m_c(c) {}
+  const ScalarT & operator[](const IndexT) const {
+    return m_c;
   }
+  const ScalarT m_c;
 };
 
-template <class L, class R>
-BinaryOp<L,plus,R> operator+(const L & left, const R & right) {
-  return BinaryOp<L,plus,R>(left,right);
+#define FA_BINARY_OP(LABEL,OPERATOR,EXPR) \
+template <class L, class R> struct LABEL {}; \
+\
+template <class L, class R> \
+struct term< LABEL< term<L>, term<R> > > { \
+  typedef term<L> TermL; \
+  typedef term<R> TermR; \
+  typedef typename TermL::ValueT ValueTL; \
+  typedef typename TermR::ValueT ValueTR; \
+  typedef typename promote<ValueTL,ValueTR>::type ValueT; \
+  term(const term<L> & left, const term<R> & right) : \
+    m_left(left), m_right(right) {} \
+  ValueT operator[](const IndexT i) const { \
+    return EXPR; \
+  } \
+  const term<L> m_left; \
+  const term<R> m_right; \
+}; \
+\
+template <class L, class R> \
+inline term< LABEL< term<L>, term<R> > > \
+OPERATOR(const L & left, const R & right) { \
+  typedef LABEL< term<L>, term<R> > TermT; \
+  return term<TermT>(left,right); \
 }
 
-struct minus {
-  static ScalarT apply(const ScalarT a, const ScalarT b) {
-    return a - b;
-  }
-};
-
-template <class L, class R>
-BinaryOp<L,minus,R> operator-(const L & left, const R & right) {
-  return BinaryOp<L,minus,R>(left,right);
-}
-
-struct mult {
-  static ScalarT apply(const ScalarT a, const ScalarT b) {
-    return a * b;
-  }
-};
-
-template <class L, class R>
-BinaryOp<L,mult,R> operator*(const L & left, const R & right) {
-  return BinaryOp<L,mult,R>(left,right);
-}
-
-struct divide {
-  static ScalarT apply(const ScalarT a, const ScalarT b) {
-    return a / b;
-  }
-};
-
-template <class L, class R>
-BinaryOp<L,divide,R> operator/(const L & left, const R & right) {
-  return BinaryOp<L,divide,R>(left,right);
-}
-
+FA_BINARY_OP(addition,       operator+, m_left[i] + m_right[i]);
+FA_BINARY_OP(subtraction,    operator-, m_left[i] - m_right[i]);
+FA_BINARY_OP(mulitiplication,operator*, m_left[i] * m_right[i]);
+FA_BINARY_OP(division,       operator/, m_left[i] / m_right[i]);
+#undef FA_BINARY_OP
 
 } // namespace fa
 
--- src/unit-tests.cpp
+++ src/unit-tests.cpp
@@ -16,6 +16,83 @@ TEST(FastArray, pass)
   }
 }
 
+TEST(FastArray, ctor_size_val)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  fa::FastArray fb(size,a);
+  ASSERT_EQ(size, fb.size());
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(a, fb[i]);
+  }
+}
+
+TEST(FastArray, set_all)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  fa::FastArray fb(size);
+  fb = a;
+  ASSERT_EQ(size, fb.size());
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(a, fb[i]);
+  }
+}
+
+TEST(FastArray, copy_ctor)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  fa::FastArray fa(size);
+  for(int i=0; i < size; ++i) {
+    fa[i] = a;
+  }
+
+  fa::FastArray fb(fa);
+
+  ASSERT_EQ(fa.size(), fb.size());
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(a, fb[i]);
+  }
+}
+
+TEST(FastArray, operator_equal)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  fa::FastArray fa(size);
+  for(int i=0; i < size; ++i) {
+    fa[i] = a;
+  }
+
+  fa::FastArray fb;
+  fb = fa;
+
+  ASSERT_EQ(size, fb.size());
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(a, fb[i]);
+  }
+}
+
+TEST(FastArray, operator_equal_with_different_size)
+{
+  const fa::IndexT a_size = 1000;
+  const fa::ScalarT a = 3;
+  fa::FastArray fa(a_size);
+  for(int i=0; i < a_size; ++i) {
+    fa[i] = a;
+  }
+
+  const fa::IndexT b_size = 10;
+  fa::FastArray fb(b_size);
+  fb = fa;
+
+  ASSERT_EQ(a_size, fb.size());
+  for(int i=0; i < a_size; ++i) {
+    ASSERT_DOUBLE_EQ(a, fb[i]);
+  }
+}
+
 TEST(FastArray, plus)
 {
   const fa::IndexT size = 1000;
@@ -241,3 +318,45 @@ TEST(FastArray, divide_equals)
   }
 }
 
+TEST(FastArray, plus_double)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  const fa::ScalarT b = 4;
+
+  fa::FastArray fa(size);
+  fa::FastArray fc(size);
+  for(int i=0; i < size; ++i) {
+    fa[i] = a;
+  }
+  const fa::ScalarT c = a + b;
+  fc = fa + b;
+
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(c, fc[i]);
+  }
+}
+
+TEST(FastArray, complext_binop)
+{
+  const fa::IndexT size = 1000;
+  const fa::ScalarT a = 3;
+  const fa::ScalarT b = 5;
+  const fa::ScalarT c = 7;
+  const fa::ScalarT d = 11;
+  const fa::ScalarT e = 13;
+
+  fa::FastArray fa(size,a);
+  fa::FastArray fb(size,b);
+  fa::FastArray fc(size,c);
+  fa::FastArray fd(size,d);
+
+  const fa::ScalarT f = a + b * c / (d + e);
+  fa::FastArray ff(size);
+  ff = fa + fb * fc / (fd + e);
+
+  ASSERT_EQ(size,ff.size());
+  for(int i=0; i < size; ++i) {
+    ASSERT_DOUBLE_EQ(f, ff[i]);
+  }
+}
